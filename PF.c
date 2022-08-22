ecce782db9abe1a0b89f157215e20d3732f783f9
blwchn <65010973@kmitl.ac.th>
2022-08-22 09:54:54 +0700

    PF Lab 2

---
+++ PF.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("Enter nuber : ");
+    scanf("%d",&a);
+    
+    i = 1;
+    while(i<=a)
+    {
+       printf("*");
+       i += 1;
+}
+}
---
+++ PF2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("Enter nuber : ");
+    scanf("%d",&a);
+    i=0;
+    for (int i = 0 ; i < a ; i++)
+        printf("*");
+}
